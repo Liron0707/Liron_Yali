1b1dbb279ee4cca31c399d0e19cf73c60e417237
noa einav <einavnoa16@gmail.com>
2026-09-08 11:47:06 +0300

    update def create screen

--- screen.py
+++ screen.py
@@ -12,38 +12,39 @@ player = pygame.Rect(50,50,consts.PLAYER_WIDTH,consts.PLAYER_HEIGHT) #pygame obj
 pygame.display.set_caption("Yali and Liron for the win")
 clock = pygame.time.Clock()
 
-
+background = pygame.image.load("background.png").convert()
 
 def draw():
     window.fill("lightpink")
     pygame.draw.rect(window,(71, 7, 7),player)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: #user click the x button
-            pygame.quit()
-            exit()
+def create_screen():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: #user click the x button
+                pygame.quit()
+                exit()
 
 
-        if event.type == pygame.KEYDOWN: #key was pressed
-            if event.key == pygame.K_UP:
-                player.y -= consts.CELL_SIZE #y is used to move objects
-            if event.key == pygame.K_DOWN:
-                player.y += consts.CELL_SIZE
-            if event.key == pygame.K_LEFT:
-                player.x -= consts.CELL_SIZE
-            if event.key == pygame.K_RIGHT:
-                player.x += consts.CELL_SIZE
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_LEFT]:
-    #     player.x -= consts.CELL_SIZE
-    # if keys[pygame.K_RIGHT]:
-    #     player.x += consts.CELL_SIZE
-    # if keys[pygame.K_UP]:
-    #     player.y -= consts.CELL_SIZE
-    # if keys[pygame.K_DOWN]:
-    #     player.y += consts.CELL_SIZE
+            if event.type == pygame.KEYDOWN: #key was pressed
+                if event.key == pygame.K_UP:
+                    player.y -= consts.CELL_SIZE #y is used to move objects
+                if event.key == pygame.K_DOWN:
+                    player.y += consts.CELL_SIZE
+                if event.key == pygame.K_LEFT:
+                    player.x -= consts.CELL_SIZE
+                if event.key == pygame.K_RIGHT:
+                    player.x += consts.CELL_SIZE
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_LEFT]:
+        #     player.x -= consts.CELL_SIZE
+        # if keys[pygame.K_RIGHT]:
+        #     player.x += consts.CELL_SIZE
+        # if keys[pygame.K_UP]:
+        #     player.y -= consts.CELL_SIZE
+        # if keys[pygame.K_DOWN]:
+        #     player.y += consts.CELL_SIZE
 
-        draw()
-        pygame.display.update()
-        clock.tick(60)
+            draw()
+            pygame.display.update()
+            clock.tick(60)
