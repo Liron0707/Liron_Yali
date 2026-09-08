b987974a6140d6e3f397761d920eea754189c735
noa einav <einavnoa16@gmail.com>
2026-09-08 12:26:35 +0300

    update def create solider

--- screen.py
+++ screen.py
@@ -4,6 +4,7 @@ import pygame
 from sys import exit
 import os
 import consts
+import solider
 
 pygame.init()
 
@@ -12,23 +13,14 @@ pygame.display.set_caption("Yali and Liron for the win")
 clock = pygame.time.Clock()
 
 
-player_image = pygame.image.load(os.path.join("bin","bibi.PNG"))
-player_image = pygame.transform.scale(player_image, (consts.PLAYER_WIDTH, consts.PLAYER_HEIGHT))
+
 flag_image = pygame.image.load(os.path.join("bin","sara.PNG"))
-flag_image = pygame.transform.scale(player_image, (consts.FLAG_WIDTH, consts.FLAG_HEIGHT))
+flag_image = pygame.transform.scale(flag_image, (consts.FLAG_WIDTH, consts.FLAG_HEIGHT))
 
-pygame.display.set_icon(player_image)
-
-class Player(pygame.Rect): #חייבת להבין מה קורה פה
-    def __init__(self):
-        pygame.Rect.__init__(self,0,0,consts.PLAYER_WIDTH,consts.PLAYER_HEIGHT) #pygame object for storing rectangular coordinates
-        self.image = player_image
-
-player = Player()
 
 def draw():
     window.fill("lightpink")
-    window.blit(player_image,player)
+    window.blit(solider.player_image, solider.player)
 
 def create_screen():
     while True:
@@ -40,13 +32,13 @@ def create_screen():
 
             if event.type == pygame.KEYDOWN: #key was pressed
                 if event.key == pygame.K_UP:
-                    player.y -= consts.CELL_SIZE #y is used to move objects
+                    solider.player.y -= consts.CELL_SIZE #y is used to move objects
                 if event.key == pygame.K_DOWN:
-                    player.y += consts.CELL_SIZE
+                    solider.player.y += consts.CELL_SIZE
                 if event.key == pygame.K_LEFT:
-                    player.x -= consts.CELL_SIZE
+                    solider.player.x -= consts.CELL_SIZE
                 if event.key == pygame.K_RIGHT:
-                    player.x += consts.CELL_SIZE
+                    solider.player.x += consts.CELL_SIZE
         # keys = pygame.key.get_pressed()
         # if keys[pygame.K_LEFT]:
         #     player.x -= consts.CELL_SIZE
