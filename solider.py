69a00cf302d85758e18d67d3b533b6c520eac20e
ariel <astra2.yes@gmail.com>
2026-09-09 10:30:25 +0300

    fixing night screen

--- solider.py
+++ solider.py
@@ -17,5 +17,5 @@ class Player(pygame.Rect): #חייבת להבין מה קורה פה
 player = Player()
 
 def solider_place_tuple(self):
-    return self.player.x/20, self.player.y/20
+    return self.x//20, self.y//20
 
